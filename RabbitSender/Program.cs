277b2d7c2ce2e99664d59ee86928057a169482c4
Daniel Besen <nielbesen@gmail.com>
2024-03-21 20:54:35 -0300

    Binding exchange with queue

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,9 +1,34 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
-ConnectionFactory connectionFactory = new ConnectionFactory();
 
 string username = Environment.GetEnvironmentVariable("RABBIT_USERNAME");
 string password = Environment.GetEnvironmentVariable("RABBIT_PASSWORD");
 
-connectionFactory.Uri = new Uri($"amqp://{username}:{password}@localhost:5672");
+//set connection
+ConnectionFactory connectionFactory = new ConnectionFactory()
+{
+    Uri = new Uri($"amqp://{username}:{password}@localhost:5672"),
+    ClientProvidedName = "Rabbit Sender APP Zero"
+};
+
+//open connection
+IConnection cnn = connectionFactory.CreateConnection();
+
+//set channel
+using (IModel channel = cnn.CreateModel()){
+
+    string exchangeName = "DemoExchange";
+    string routingKey = "demo-routing-key";
+    string queueName = "DemoQueue";
+
+    //set exchange
+    channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+    channel.QueueDeclare(queueName, false, false, false, null);
+    channel.QueueBind(queueName, exchangeName, routingKey);
+
+};
+
+
+
+
