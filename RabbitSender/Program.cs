466e7184f5f2fee67eaf4b9fc4c836bb7b19ea7d
Daniel Besen <nielbesen@gmail.com>
2024-03-21 21:44:27 -0300

    RabbitSender implemented

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,5 +1,8 @@
-﻿using RabbitMQ.Client;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitSender.Models;
 using System.Text;
+using System.Text.Json.Serialization;
 
 
 string username = Environment.GetEnvironmentVariable("RABBIT_USERNAME");
@@ -22,10 +25,25 @@ using (IModel channel = cnn.CreateModel()){
     string routingKey = "demo-routing-key";
     string queueName = "DemoQueue";
 
+    //set exchange, set queue & bind exchange and queue
     channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
     channel.QueueDeclare(queueName, false, false, false, null);
     channel.QueueBind(queueName, exchangeName, routingKey);
 
+    User user = new User()
+    {
+        Age = 25,
+        Email = "nielb@gmail.com",
+        FirstName = "Daniel",
+        LastName = "de Sá Besen"
+    };
+
+    string jsonMessage = JsonConvert.SerializeObject(user);
+    var encondedMessage = Encoding.UTF8.GetBytes(jsonMessage);
+
+    channel.BasicPublish(exchangeName, routingKey, null, encondedMessage);
+    channel.Close();
+    cnn.Close();
 };
 
 
